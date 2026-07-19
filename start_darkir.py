5ff18b90aae20412243c2dbc4925f777248a37b2
mohamedmagdy9977 <m7mdcena77@gmail.com>
2026-07-19 17:25:06 +0300

    Simple Use for DarkIR

---
+++ start_darkir.py
@@ -0,0 +1,18 @@
+from DarkIR.download_model import (
+    LoadedModel,
+    load_ready_model,
+    shutdown_ready_model,
+)
+from DarkIR.inference import run_low_light_img_inference
+from DarkIR.inference_video import run_low_light_video_inference
+
+
+# Load the model
+model: LoadedModel = load_ready_model()
+
+# Run the inference
+run_low_light_img_inference(model, "Source/input.png", "Target/output.png")
+run_low_light_video_inference(model, "Source/input.mp4", "Target/output.mp4")
+
+# Shutdown the model
+shutdown_ready_model()
